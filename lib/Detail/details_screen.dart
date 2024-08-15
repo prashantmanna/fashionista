761dacee46c3777e2f2dfce647a40d6255b34c6f
prashant <manprashant99@gmail.com>
2024-08-15 12:56:04 +0530

    first commit

--- lib/Detail/details_screen.dart
+++ lib/Detail/details_screen.dart
@@ -3,7 +3,6 @@ import 'package:fashionista/Detail/details_app_bar.dart';
 import 'package:fashionista/Detail/item_details.dart';
 import 'package:fashionista/models/products.dart';
 import 'package:fashionista/utils/constants.dart';
-import 'package:fashionista/widgets/images_slider.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatefulWidget {
@@ -27,22 +26,22 @@ class _DetailsScreenState extends State<DetailsScreen> {
       body: SafeArea(
         child: Column(
             children: [
-              DetailsAppBar(),
+              const DetailsAppBar(),
               MyImageSlider(image: widget.p2.image, onChange: (index) {
                 setState(() {
                   currentImage = index;
                 });
               },),
-              SizedBox(height: 10,),
+              const SizedBox(height: 10,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: List.generate(5, (index) =>
                     AnimatedContainer(
                       duration:
-                      Duration(milliseconds: 3000),
+                      const Duration(milliseconds: 3000),
                       width: currentImage == index ? 15 : 5,
                       height: 8,
-                      margin: EdgeInsets.only(right: 3),
+                      margin: const EdgeInsets.only(right: 3),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           color: currentImage == index ? Colors.blue : Colors
@@ -55,7 +54,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
                 ),
 
               ),
-              SizedBox(height: 20,),
+              const SizedBox(height: 20,),
               Container(width: double.infinity, decoration: const BoxDecoration(
                 color: constants.mainColor,
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(40),
@@ -67,22 +66,26 @@ class _DetailsScreenState extends State<DetailsScreen> {
                   children: <Widget>[
                     ItemDetails(p3: widget.p2,),
                     const SizedBox(height: 10,),
-                    Text("Colors",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
-                    SizedBox(height: 10,),
+                    const Text("Colors",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+                    const SizedBox(height: 10,),
                     Row(
                       children: List.generate(widget.p2.colors.length, (index)=> GestureDetector(
-                        onTap: (){},
+                        onTap: (){
+                          setState(() {
+                            currentColor = index;
+                          });
+                        },
                         child: AnimatedContainer(
-                          duration: Duration(milliseconds: 300),
-                          width: 35,
-                          height: 35,
+                          duration: const Duration(milliseconds: 300),
+                          width: 40,
+                          height: 40,
                           decoration: BoxDecoration(
                             shape: BoxShape.circle,
                             color: currentColor == index?Colors.white:widget.p2.colors[index],
                             border:currentColor == index?Border.all(color: widget.p2.colors[index]):null,
                           ),
-                          padding: currentColor == index? EdgeInsets.all(2):null,
-                          margin: EdgeInsets.only(right: 10),
+                          padding: currentColor == index? const EdgeInsets.all(2):null,
+                          margin: const EdgeInsets.only(right: 10),
                           child: Container(
                             width: 35,
                             height: 35,
